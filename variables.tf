42b7c47b7c96508c3b29c5e2e789410c0b1585fa
Felipe Arciniegas <felipe.arciniegas@pragma.com.co>
2026-06-09 17:41:39 -0500

    fix: add prod to environment validation in variables.tf

--- variables.tf
+++ variables.tf
@@ -3,8 +3,8 @@ variable "environment" {
   type        = string
   
   validation {
-    condition     = contains(["dev", "qa", "pdn"], var.environment)
-    error_message = "El entorno debe ser uno de: dev, qa, pdn."
+    condition     = contains(["dev", "qa", "pdn", "prod"], var.environment)
+    error_message = "El entorno debe ser uno de: dev, qa, pdn, prod."
   }
 }
 
